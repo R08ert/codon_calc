3f9390d6832d1867992b47444d1f6a722ad7ae6e
Laura <laura.morrissey@gmail.com>
2014-11-24 20:56:03 -0600

    add program options

---
+++ main.py
@@ -0,0 +1,20 @@
+from optparse import OptionParser
+from parse_file import SequenceReader
+
+def main():
+    usage = "usage: %prog [-i] csvfile [-o] outputfilename"
+    parser = OptionParser(usage)
+    parser.add_option("-i", "--inputfilename", dest = "inputfilename" ,
+                  help = "read sequence from csv input FILE", metavar = "FILE")
+    parser.add_option("-o", "--outputfile", dest = "outputfilename",
+                  help="write sequence to pdf output file")
+
+    (options,args)= parser.parse_args()
+
+
+    seqio = SequenceReader(options.inputfilename, options.outputfilename)
+    seqio.read_file()
+
+
+if __name__ == "__main__":
+    main()
