212a236c46c7a890a18eb2bacb3dfb1b65e7052b
Laura <laura.morrissey@gmail.com>
2014-11-24 18:54:55 -0600

    add functionality for reading sequence/checking for errors

---
+++ parse_file.py
@@ -0,0 +1,29 @@
+__author__ = 'Laura'
+import csv
+import re
+
+class SequenceReader:
+    def __init__(self, inputfilename = None, outputfilename=None):
+        self.inputfile = inputfilename
+        self.outputfile = outputfilename
+
+    def read_file(self):
+        with open(self.inputfile, 'rb') as csvfile:
+            dialect = csv.Sniffer().sniff(csvfile.readline(),[',',';','\t'])
+            csvfile.seek(0)
+            sequencereader = csv.reader(csvfile, dialect)
+            for sequence in sequencereader:
+                self.validate_input(sequence)
+                # print sequence
+
+    def validate_input(self, sequence):
+        if len(sequence[0]) % 3 is  not 0:
+            print "Error in sequence " + str(sequence)
+            print "Length is: " + str(len(sequence[0]))
+        if not re.match("^[AaGgCcTtUu]*$", sequence[0]):
+            print "invalid value in sequence " + str(sequence[0])
+
+
+
+
+
